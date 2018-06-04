b649133e3f8f45651d8ab8c18f4613eafd0508f5
Milorad Bozic <miloradbozic29@gmail.com>
2018-06-04 10:47:13 +0200

    fix pom file

--- src/main/java/io/milo/rateapp/core/worker/ImportUsers.java
+++ src/main/java/io/milo/rateapp/core/worker/ImportUsers.java
@@ -9,6 +9,7 @@ import io.milo.rateapp.core.client.UinamesUserClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -16,8 +17,9 @@ import org.springframework.stereotype.Component;
 /***
  * Imports users from external source
  */
+
 @Component
-@ConditionalOnProperty("worker.importUsers.run")
+@ConditionalOnExpression("${worker.importUsers.run:true}")
 public class ImportUsers {
 
     @Autowired
@@ -28,11 +30,11 @@ public class ImportUsers {
 
     @Scheduled(fixedRate = 5000)
     public void importSingle() {
-        /*User user = UinamesUserClient.getClient().getSingle(); // @todo use dependency injection
+        User user = UinamesUserClient.getClient().getSingle();
         try {
             this.userRepository.create(user);
         } catch (IOException e) {
             log.error("Error while inserting user {} to ES:\n {}", user.toString(), e.getMessage());
-        }*/
+        }
     }
 }
